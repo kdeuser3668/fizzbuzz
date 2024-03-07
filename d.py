505ffe1026a67f95cbef9636fcc5498be6c1c723
kdeuser3668 <112040794+kdeuser3668@users.noreply.github.com>
2024-03-07 12:44:39 -0600

    Add files via upload

---
+++ d.py
@@ -0,0 +1,13 @@
+# run program that prints numbers 1-100
+# multiples of 3 print fizz
+# mulitples of 5 print buzz
+# mulitples of 3 & 5 print fizzbuzz
+for n in range(1,101):
+    if (n % 3 == 0 and n % 5 == 0):
+        print("fizzbuzz")
+    elif (n % 3 == 0):
+        print("fizz")
+    elif (n % 5 == 0):
+        print("buzz")
+    else:
+        print(n)
